b418a26785183d16ab8257562da321bc80fde16d
Imranullah Khan <129750188+ImranullahKhann@users.noreply.github.com>
2024-04-18 15:43:58 +0500

    Exercise 03 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    wordLength = word.length;
+    let reverse = "";
+    for (let i = 1; i <= wordLength; i++)
+    {
+        reverse += word.charAt(wordLength - i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
