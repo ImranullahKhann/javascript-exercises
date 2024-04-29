676b6356b5a701bd54cff48c5eb5cf80af6f67fc
Imranullah Khan <129750188+ImranullahKhann@users.noreply.github.com>
2024-04-29 11:13:41 +0500

    Exercise 09 solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,40 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  // Traverse through the string
+  let newStr = "";
+  for (letter of str)
+    // if letter is alphabetic
+    if (isAlpha(letter))
+      // concatenate it to a string
+      newStr += letter;
+  // turn into lowercase for easier comparison
+  newStr = newStr.toLowerCase();
+  // save the reverse of above string in a new variable
+  let reverseStr = reverseString(newStr);
+  // if both are equal 
+  if (newStr === reverseStr)
+    //return true
+    return true;
+  // else
+  else
+    // return false
+    return false;
 };
+
+// Function to check if letter is alphabetic
+function isAlpha(str) {
+  // regex that includes alphabets (lower and upper case) and numbers
+  let regex = /^[a-zA-Z0-9]+$/;
+  return regex.test(str);
+}
+
+// Function to reverse string 
+function reverseString(str) { 
+  // splits the string into array of letters
+  // reverses the array
+  // joins it again to get the reversed string
+  const reverseStr =  str.split('').reverse().join(''); 
+    return reverseStr; 
+} 
 
 // Do not edit below this line
 module.exports = palindromes;
