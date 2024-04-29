ef27d3211fdeb1e5e5645dee375a407a6927886f
Imranullah Khan <129750188+ImranullahKhann@users.noreply.github.com>
2024-04-29 10:31:49 +0500

    Exercise 08 solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+// using rest parameters
+const sum = function(arr) {
+  let total = 0;
+  for (num of arr)
+    total += num;
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (num of arr)
+    product *= num;
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	let result = num;
+  for (let i = 1; i < power; i++)
+  {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  if (num > 1)
+  {
+    for (let i = 2; i <= num; i++)
+    {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
