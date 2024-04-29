cb3cdb7bd79860d91b6a27c5ace9a99e806f3790
Imranullah Khan <129750188+ImranullahKhann@users.noreply.github.com>
2024-04-29 12:32:14 +0500

    Exercise 12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((last, current) => {
+    // Giving age if  yearOfDeath is not provided
+    currentYear = new Date().getFullYear();
+    if (!last.yearOfDeath) last.yearOfDeath = currentYear;
+    if (!current.yearOfDeath) current.yearOfDeath = currentYear;
+    // if last oldest person is older than the current person
+    if (last.yearOfDeath - last.yearOfBirth > current.yearOfDeath - current.yearOfBirth) return last;
+    else return current;
+  });
 };
 
 // Do not edit below this line
